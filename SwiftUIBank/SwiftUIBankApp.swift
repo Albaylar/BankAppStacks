b44bc08b9893cb1e5921f5734f105d7156d0bc58
Albaylar <furkandenizalbaylar@gmail.com>
2022-07-26 13:59:16 +0300

    Initial Commit

---
+++ SwiftUIBank/SwiftUIBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBankApp.swift
+//  SwiftUIBank
+//
+//  Created by Furkan Deniz Albaylar on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
